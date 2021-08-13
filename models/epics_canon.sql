99bea6adc42a40d339866ecf8ab233efa046fb9f
Makan Azarshahy <makan.azarshahy@pulse42.io>
2021-08-13 10:12:15 +0000

    added indexing macros, and calls for appropriate tables

--- models/epics_canon.sql
+++ models/epics_canon.sql
@@ -1,7 +1,9 @@
 {{
   config({
-    "post-hook": 'create index if not exists {{ this.name }}__index_on_epic_id on {{ this }} ("epic_id")'
-    })
+    "post-hook": [
+      "{{create_index(this, 'epic_id')}}",
+    ]
+  })
 }}
 SELECT
     JIR.id AS epic_id,
