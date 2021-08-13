2f73a4b181c21696eca2ddaf0672ded7e3b1b427
Makan Azarshahy <makan.azarshahy@pulse42.io>
2021-08-13 08:12:07 +0000

    created base script for issue script

--- models/epics.sql
+++ models/epics.sql
@@ -1,48 +1,36 @@
--- SELECT
---     JIR.id AS epic_id,
---     JIR.issue::json->>'key' AS key,
---     'parent_id' AS parent_id,
---     'epic_name' AS epic_name,
---     (JIR.issue::json->>'fields')::json->>'timespent' AS time_spent,
---     ((JIR.issue::json->>'fields')::json->>'project')::json->>'id' AS project_id,
---     ((JIR.issue::json->>'fields')::json->>'issueType')::json->>'name' AS issue_type,
---     ((JIR.issue::json->>'fields')::json->>'resolution')::json->>'name' AS resolution,
---     (JIR.issue::json->>'fields')::json->>'workratio' AS work_ratio,
---     (JIR.issue::json->>'fields')::json->>'lastViewed' AS last_viewed_date,
---     (JIR.issue::json->>'fields')::json->>'resolutiondate' AS resolution_date,
---     (JIR.issue::json->>'fields')::json->>'created' AS created_date,
---     (JIR.issue::json->>'fields')::json->>'duedate' AS due_date,
---     (JIR.issue::json->>'fields')::json->>'updated' AS last_updated_date,
---     ((JIR.issue::json->>'fields')::json->>'status')::json->>'name' AS status,
---     (JIR.issue::json->>'fields')::json->>'summary' AS summary,
---     ((JIR.issue::json->>'fields')::json->>'description')::json->>'content' AS description,
---     ((JIR.issue::json->>'fields')::json->>'creator')::json->>'accountId' AS reporter_id,
---     ((JIR.issue::json->>'fields')::json->>'assignee')::json->>'accountId' AS assignee_id,
---     (((JIR.issue::json->>'fields')::json->>'priority')::json->>'priority')::json->>'name' AS priority,
---     (JIR.issue::json->>'fields')::json->>'environment' AS environment,
---     'feature_id' AS feature_id,
---     'last_update_by_id' AS last_update_by_id,
---     JIR.issue::json->>'self' AS self_url,
---     'JIRA' AS data_source
--- FROM jira_issue_raw AS JIR
--- WHERE ((issue::json->> 'fields')::json->>'issuetype')::json->>'name' = 'Epic'
--- LIMIT 10;
+SELECT
+    JIR.id AS epic_id,
+    JIR.issue::json->'key' AS key,
+    'epic_name' AS epic_name,
+    JIR.issue::json->'fields'->'timespent' AS time_spent,
+    JIR.issue::json->'fields'->'project'->'id' AS project_id,
+    JIR.issue::json->'fields'->'issueType'->'name' AS issue_type,
+    JIR.issue::json->'fields'->'resolution'->'name' AS resolution,
+    JIR.issue::json->'fields'->'workratio' AS work_ratio,
+    JIR.issue::json->'fields'->'lastViewed' AS last_viewed_date,
+    JIR.issue::json->'fields'->'resolutiondate' AS resolution_date,
+    JIR.issue::json->'fields'->'created' AS created_date,
+    JIR.issue::json->'fields'->'duedate' AS due_date,
+    JIR.issue::json->'fields'->'updated' AS last_updated_date,
+    JIR.issue::json->'fields'->'status'->'name' AS status,
+    JIR.issue::json->'fields'->'summary' AS summary,
+    JIR.issue::json->'fields'->'description'->'content' AS description,
+    JIR.issue::json->'fields'->'creator'->'accountId' AS reporter_id,
+    JIR.issue::json->'fields'->'assignee'->'accountId' AS assignee_id,
+    JIR.issue::json->'fields'->'priority'->'priority'->'name' AS priority,
+    JIR.issue::json->'fields'->'environment' AS environment,
+    'feature_id' AS feature_id,
+    issue::json
+        ->'changelog'
+        ->'histories'
+        ->(json_array_length(issue::json->'changelog'->'histories')-1)
+        ->'author'
+        ->'accountId' AS last_update_by_id,
+    JIR.issue::json->'self' AS self_url,
+    'JIRA' AS data_source
+FROM jira_issue_raw AS JIR
+WHERE issue::json->'fields'->'issuetype'->>'name' = 'Epic'
+LIMIT 10;
 
--- -- epic_id
--- -- key
--- -- - name: epic_name
--- -- summary
--- -- reporter_id
--- -- assignee_id
--- -- priority
--- -- - name: feature_id
--- -- project_id
--- -- resolution
--- -- resolution_date
--- -- created_date
--- -- closed_date
--- -- due_date
--- -- last_updated_date
--- -- - name: last_update_by_id
--- -- self_url
--- -- data_source
+-- - name: status
+-- - name: url
--- models/issue_field_history.sql
+++ models/issue_field_history.sql
@@ -3,14 +3,13 @@ SELECT
         '_',
         item->>'fieldId',
         history->>'created',
-        (history->>'author')::json->>'accountId') AS issue_field_history_id,
-    item->>'fieldId' AS field_id,
-    JIR.issue::json->>'id' AS issue_id,
-    history->>'created' AS time_change,
-    item->>'toString' AS new_value,
-    (history->>'author')::json->>'accountId' AS author_id
+        history->'author'->>'accountId') AS issue_field_history_id,
+    item->'fieldId' AS field_id,
+    JIR.id AS issue_id,
+    history->'created' AS time_change,
+    item->'toString' AS new_value,
+    history->'author'->'accountId' AS author_id
 FROM jira_issue_raw AS JIR
-CROSS JOIN JSON_ARRAY_ELEMENTS(((issue::json->>'changelog')::json->>'histories')::json) AS history
-CROSS JOIN JSON_ARRAY_ELEMENTS((history->>'items')::json) AS item
-WHERE
-    json_array_length(((issue::json->>'changelog')::json->>'histories')::json) > 0;
+CROSS JOIN JSON_ARRAY_ELEMENTS(issue::json->'changelog'->'histories') AS history
+CROSS JOIN JSON_ARRAY_ELEMENTS(history->'items') AS item
+WHERE json_array_length(issue::json->'changelog'->'histories') > 0;
